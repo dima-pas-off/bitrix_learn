a083d16d1acc8318de92b0c2734c41be3bf0092d
Dima Pashkevich <dima.paskevic.off@gmail.com>
2022-02-18 09:24:42 +0300

    Update task

--- local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
+++ local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
@@ -1,9 +1,15 @@
 <?
-
-
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+
+    $comp = $this->__component;
+
     if($arParams["SPECIAL_DATE"] === 'Y') {
         $activeFromLastNews = $arResult["ITEMS"][0]["ACTIVE_FROM"];
-        $APPLICATION->SetPageProperty("specialdate", $activeFromLastNews);
+        $arResult["TITLE"] = $activeFromLastNews;
+        $comp->SetResultCacheKeys(["TITLE"]);
     }
-?>
+
+
+
+
